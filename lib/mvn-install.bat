2ed3ae3d5c4a78f279c78b394340bea20cdfd9fd
akausinis <andrius.kausinis@singleton-labs.lt>
2016-02-09 11:00:38 +0200

    Libs installer bat fixes.

--- lib/mvn-install.bat
+++ lib/mvn-install.bat
@@ -3,18 +3,18 @@
 setlocal
 
 if "%M2_HOME%" == "" goto try_maven_home
-set MVN=%M2_HOME%\bin\mvn.bat
+set MVN=%M2_HOME%\bin\mvn
 if not exist "%MVN%" goto try_maven_home
 goto run_mvn
 
 :try_maven_home
 if "%MAVEN_HOME%" == "" goto try_path
-set MVN=%MAVEN_HOME%\bin\mvn.bat
+set MVN=%MAVEN_HOME%\bin\mvn
 if not exist "%MVN%" goto try_path
 goto run_mvn
 
 :try_path
-set MVN=C:\maven\bin\mvn.bat
+set MVN=C:\maven\bin\mvn
 
 :run_mvn
 
