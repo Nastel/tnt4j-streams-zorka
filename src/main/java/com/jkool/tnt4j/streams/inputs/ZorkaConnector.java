adad243b16d688b3d5fe8693b8a9a51d387d8f0e
akausinis <andrius.kausinis@singleton-labs.lt>
2016-02-18 13:34:13 +0200

    fixed file headers

--- src/main/java/com/jkool/tnt4j/streams/inputs/ZorkaConnector.java
+++ src/main/java/com/jkool/tnt4j/streams/inputs/ZorkaConnector.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2015 jKool, LLC. All Rights Reserved.
+ * Copyright 2014-2016 JKOOL, LLC.
  *
- * This software is the confidential and proprietary information of
- * jKool, LLC. ("Confidential Information").  You shall not disclose
- * such Confidential Information and shall use it only in accordance with
- * the terms of the license agreement you entered into with jKool, LLC.
+ * This file is part of TNT4J-Streams-Zorka.
  *
- * JKOOL MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
- * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. JKOOL SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
+ * TNT4J-Streams-Zorka is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * CopyrightVersion 1.0
+ * TNT4J-Streams-Zorka is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with TNT4J-Streams-Zorka.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.jkool.tnt4j.streams.inputs;
--- src/main/java/com/jkool/tnt4j/streams/utils/ZorkaConstants.java
+++ src/main/java/com/jkool/tnt4j/streams/utils/ZorkaConstants.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2014-2016 JKOOL, LLC.
+ *
+ * This file is part of TNT4J-Streams-Zorka.
+ *
+ * TNT4J-Streams-Zorka is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TNT4J-Streams-Zorka is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TNT4J-Streams-Zorka.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.jkool.tnt4j.streams.utils;
 
 /**
