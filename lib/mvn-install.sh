15abe7d5cd2a95064c6680150b4b6e85350e91ee
akausinis <andrius.kausinis@singleton-labs.lt>
2016-03-09 16:30:05 +0200

    CRLF -> CR for bash scripts

--- lib/mvn-install.sh
+++ lib/mvn-install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 echo installing libraries required by Zorka...
-#mvn install:install-file -Dfile=zico-util.jar -DgroupId=zorka -DartifactId=zico-util -Dversion=1.0.0 -Dpackaging=jar
+
+mvn install:install-file -Dfile="${BASH_SOURCE%/*}/zico-util.jar" -DgroupId=zorka -DartifactId=zico-util -Dversion=1.0.0 -Dpackaging=jar
 
 echo DONE!
 
-read -p "Press [Enter] key to exit..."
+#read -p "Press [Enter] key to exit..."
