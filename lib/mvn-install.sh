7c15319bada0e35d47c245c24768749411a0cd40
akausinis <andrius.kausinis@singleton-labs.lt>
2016-03-10 10:18:52 +0200

    Cleanup

--- lib/mvn-install.sh
+++ lib/mvn-install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo installing libraries required by Zorka...
-
 mvn install:install-file -Dfile="${BASH_SOURCE%/*}/zico-util.jar" -DgroupId=zorka -DartifactId=zico-util -Dversion=1.0.0 -Dpackaging=jar
 
 echo DONE!
