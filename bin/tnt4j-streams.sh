eecd64783aab33ccbe46dd4adcd7a1a6357a82d7
akausinis <andrius.kausinis@singleton-labs.lt>
2016-07-18 15:33:37 +0300

    maven gpg plugin initiation rework. now gpg is only invoked by adding CLI argument -DperformRelease=true

--- bin/tnt4j-streams.sh
+++ bin/tnt4j-streams.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 RUNDIR=`pwd`
-CLASSPATH="$RUNDIR/../*:$RUNDIR/../libs/*"
+CLASSPATH="$RUNDIR/../*:$RUNDIR/../lib/*"
 TNT4JOPTS=-Dtnt4j.config="$RUNDIR/../config/tnt4j.properties"
 LOG4JOPTS=-Dlog4j.configuration="file:%RUNDIR%/../config/log4j.properties"
 #LOGBACKOPTS=-Dlogback.configurationFile="file:%RUNDIR%/../config/logback.xml"
--- bin/zorka-attach.bat
+++ bin/zorka-attach.bat
@@ -1,6 +1,6 @@
 @echo off
 set RUNDIR=%~p0
 set PATH=%PATH%;"%JAVA_HOME%\jre\bin\"
-set CLASSPATH=%RUNDIR%..\*";"%RUNDIR%..\libs\*;"%JAVA_HOME%\lib\tools.jar"
+set CLASSPATH=%RUNDIR%..\*";"%RUNDIR%..\lib\*;"%JAVA_HOME%\lib\tools.jar"
 java -classpath %CLASSPATH% com.jkoolcloud.tnt4j.streams.utils.ZorkaAttach %1 %2 
 call %RUNDIR%\tnt4j-streams.bat %3
