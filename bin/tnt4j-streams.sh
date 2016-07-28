e44e2e7bfffcb64dd4149bba07743ae88e4208ac
akausinis <andrius.kausinis@singleton-labs.lt>
2016-07-28 09:57:38 +0300

    sh fix

--- bin/tnt4j-streams.sh
+++ bin/tnt4j-streams.sh
@@ -2,6 +2,6 @@
 RUNDIR=`pwd`
 CLASSPATH="$RUNDIR/../*:$RUNDIR/../lib/*"
 TNT4JOPTS=-Dtnt4j.config="$RUNDIR/../config/tnt4j.properties"
-LOG4JOPTS=-Dlog4j.configuration="file:%RUNDIR%/../config/log4j.properties"
-#LOGBACKOPTS=-Dlogback.configurationFile="file:%RUNDIR%/../config/logback.xml"
+LOG4JOPTS=-Dlog4j.configuration="file:$RUNDIR/../config/log4j.properties"
+#LOGBACKOPTS=-Dlogback.configurationFile="file:$RUNDIR/../config/logback.xml"
 java ${LOG4JOPTS} ${TNT4JOPTS} -classpath ${CLASSPATH} com.jkoolcloud.tnt4j.streams.StreamsAgent $*
