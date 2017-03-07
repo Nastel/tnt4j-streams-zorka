447fef1108db7f88171e4f2f36878466ac46df9b
akausinis <andrius.kausinis@singleton-labs.lt>
2017-03-07 10:12:02 +0200

    run script fix

--- bin/zorka-attach.sh
+++ bin/zorka-attach.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 RUNDIR=`pwd`
+TOOLS_PATH="$JAVA_HOME/lib/tools.jar"
 export PATH=$PATH;"$JAVA_HOME/jre/bin/"
-CLASSPATH="$RUNDIR/../*:$RUNDIR/../lib/*";"$JAVA_HOME/lib/tools.jar"
+CLASSPATH="$RUNDIR/../*:$RUNDIR/../lib/*:$TOOLS_PATH"
 java -classpath ${CLASSPATH} com.jkoolcloud.tnt4j.streams.utils.ZorkaAttach $1 $2
 tnt4j-streams.sh $3
