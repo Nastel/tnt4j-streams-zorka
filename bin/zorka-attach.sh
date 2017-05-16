8b7b189f58088ac43066b8e813bc40649c6e2c5e
akausinis <andrius.kausinis@singleton-labs.lt>
2017-05-16 11:04:58 +0300

    bash scripts fixed.

--- bin/zorka-attach.sh
+++ bin/zorka-attach.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
-SCRIPTPATH=`realpath $0`
-RUNDIR=`dirname $SCRIPTPATH`
+if command -v realpath >/dev/null 2>&1; then
+    SCRIPTPATH=`dirname $(realpath $0)`
+else
+    SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+fi
+
+RUNDIR=`pwd`
 TOOLS_PATH="$JAVA_HOME/lib/tools.jar"
 export PATH=$PATH;"$JAVA_HOME/jre/bin/"
-CLASSPATH="$RUNDIR/../*:$RUNDIR/../lib/*:$TOOLS_PATH"
+CLASSPATH="$SCRIPTPATH/../*:$SCRIPTPATH/../lib/*:$TOOLS_PATH"
 java -classpath ${CLASSPATH} com.jkoolcloud.tnt4j.streams.utils.ZorkaAttach $1 $2
 tnt4j-streams.sh $3
