ebb49536b4b7b75e71a5b076850e2227cdbf35ee
akausinis <andrius.kausinis@singleton-labs.lt>
2017-05-22 14:24:06 +0300

    env variable renamed not to conflict with system var name.

--- bin/zorka-attach.bat
+++ bin/zorka-attach.bat
@@ -2,10 +2,10 @@
 setlocal
 
 set RUNDIR=%~p0
-set PATH=%PATH%;"%JAVA_HOME%\jre\bin\"
+set PATH="%PATH%;%JAVA_HOME%\jre\bin\"
 set TOOLS_PATH="%JAVA_HOME%\lib\tools.jar"
-set CLASSPATH=%CLASSPATH%";"%RUNDIR%..\*";"%RUNDIR%..\lib\*";"%TOOLS_PATH%
+set LIBPATH="%RUNDIR%..\*;%RUNDIR%..\lib\*;%TOOLS_PATH%"
 
 @echo on
-java -classpath "%CLASSPATH%" com.jkoolcloud.tnt4j.streams.utils.ZorkaAttach %1 %2
+java -classpath "%LIBPATH%" com.jkoolcloud.tnt4j.streams.utils.ZorkaAttach %1 %2
 call %RUNDIR%\tnt4j-streams.bat %3
--- bin/zorka-attach.sh
+++ bin/zorka-attach.sh
@@ -7,7 +7,7 @@ fi
 
 RUNDIR=`pwd`
 TOOLS_PATH="$JAVA_HOME/lib/tools.jar"
-export PATH=$PATH;"$JAVA_HOME/jre/bin/"
-CLASSPATH="$SCRIPTPATH/../*:$SCRIPTPATH/../lib/*:$TOOLS_PATH"
-java -classpath ${CLASSPATH} com.jkoolcloud.tnt4j.streams.utils.ZorkaAttach $1 $2
+export PATH="$PATH:$JAVA_HOME/jre/bin/"
+LIBPATH="$SCRIPTPATH/../*:$SCRIPTPATH/../lib/*:$TOOLS_PATH"
+java -classpath "$LIBPATH" com.jkoolcloud.tnt4j.streams.utils.ZorkaAttach $1 $2
 tnt4j-streams.sh $3
