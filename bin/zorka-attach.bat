0dd181043a31601aba4054bc1062d727a91e8e69
akausinis <andrius.kausinis@singleton-labs.lt>
2017-09-19 13:31:32 +0300

    made RUNDIR to resolve complete path with drive letter

--- bin/zorka-attach.bat
+++ bin/zorka-attach.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set RUNDIR=%~p0
+set RUNDIR=%~dp0
 set PATH="%PATH%;%JAVA_HOME%\jre\bin\"
 set TOOLS_PATH="%JAVA_HOME%\lib\tools.jar"
 set LIBPATH="%RUNDIR%..\*;%RUNDIR%..\lib\*;%TOOLS_PATH%"
